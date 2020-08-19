728740a9ee339725ee1b725ff97b36cd51c44d08
Carlos Torres <59813315+rocatoro18@users.noreply.github.com>
2020-08-18 20:28:50 -0600

    Add files via upload

--- JavaScript/HolaMundo/js/36_fetch.js
+++ JavaScript/HolaMundo/js/36_fetch.js
@@ -4,18 +4,30 @@
 
 var div_usuarios = document.querySelector("#usuarios");
 var div_janet = document.querySelector("#janet");
+var div_profesor = document.querySelector("#profesor");
 
 
     getUsuarios().then(data => data.json())
     .then(users => {
         listadoUsuarios(users.data);
 
+        return getInfo();
+    })
+    .then(data => {
+
+        div_profesor.innerHTML = data;
+
         return getJanet();
     })
     .then(data => data.json())
     .then(user => {
         mostrarJanet(user.data);
+
+    })
+    .catch(error => {
+        alert("Error en las peticiones");
     });
+    
 
 function getUsuarios(){
     return fetch('https://reqres.in/api/users');
@@ -23,6 +35,31 @@ function getUsuarios(){
 
 function getJanet(){
     return fetch('https://reqres.in/api/users/2');
+}
+
+function getInfo(){
+    var profesor = {
+        nombre: 'Carlos',
+        apellidos: 'Torres',
+        url: 'https://www.google.com.mx/'
+    };
+
+    return new Promise((resolve, reject) => {
+        var profesor_string = "";
+
+        setTimeout(function(){
+            profesor_string = JSON.stringify(profesor);
+
+            if(typeof profesor_string != 'string' || profesor_string == ''){
+                return reject('error');
+            }else{
+                return resolve(profesor_string);
+            }
+
+        }, 3000);
+
+    });
+
 }
 
 
---
+++ JavaScript/HolaMundo/js/37_try_catch.js
@@ -0,0 +1,13 @@
+'use strict'
+
+try{
+    var year = 2020;
+    alert(year);
+    var vector = new Array(1000000000000000000000000000000000);
+    console.log(decodeURIComponent("https://www.google.com.mx"));
+
+}catch(error){
+    console.log(error);
+    alert("A ocurrido un error en el código");
+}
+
