359f534212a67a3eca0ffe6f7dc4e98602ca658e
Carlos Torres <59813315+rocatoro18@users.noreply.github.com>
2020-08-24 20:45:58 -0600

    Add files via upload

---
+++ JavaScript/aprendiendo_jquery/js/05_ajax.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    
+    // Load
+
+    // $("#datos").load("https://reqres.in/");
+
+    // Get
+
+    $.get("https://reqres.in/api/users", {page: 2}, function(response){
+        response.data.forEach((element, index) => {
+            $("#datos").append("<p>"+element.first_name + " " + element.last_name +"</p>");
+        });
+    });
+
+    // Post
+
+    $("#formulario").submit(function(e){
+        e.preventDefault();
+        var usuario = {
+            name: $('input[name="name"]').val(),
+            web: $('input[name="web"]').val()
+        };
+        /*
+        $.post($(this).attr("action"), usuario, function(response){
+            console.log(response);
+        }).done(function(){
+            alert("Usuario Agregado Correctamente");
+        });
+        */
+       
+         // Metodo $.ajax (Es mejor que el otro porque es mas configurable)
+
+         $.ajax({
+            type: 'POST',
+            // NO HACEN FALTA ESTAS PROPIEDADES dataType: 'json',
+            // NO HACEN FALTA ESTAS PROPIEDADES contentType: 'application/json'
+            url: $(this).attr("action"),
+            data: usuario,
+            beforeSend: function(){
+                console.log("Enviando Usuario...")
+            },
+            success: function(response){
+                console.log(response);
+            },
+            error: function(){
+                console.log("A ocurrido un error");
+            },
+            timeout: 2000
+        });
+
+        return false;
+    
+    });
+
+});
