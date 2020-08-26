1e9a5f3ea42d0c94cb5b5fd1f8d8437a1f190709
Carlos Torres <59813315+rocatoro18@users.noreply.github.com>
2020-08-25 21:07:08 -0600

    Add files via upload

---
+++ JavaScript/aprendiendo_jquery/js/06_interface.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+    // Mover elemento por la pagina
+
+    $(".elemento").draggable();
+
+    // Redimensionar elementos
+
+    $(".elemento").resizable();
+
+    // Seleccionar elementos
+
+    // $(".lista-seleccionable").selectable();
+
+    // Seleccionar y ordenar elementos
+
+    $(".lista-seleccionable").sortable({
+        update: function(event, ui){
+            console.log("Ha cambiado la lista");
+        }
+    });
+
+    // Drop
+
+    $("#elemento-movido").draggable();
+    $("#area").droppable({
+        drop: function(){
+            console.log("Has soltado algo dentro del area");
+        }
+    });
+
+    // Efectos
+
+    $("#mostrar").click(function(){
+        // CHECAR DOCUMENTACION PARA VER MAS EFECTOS
+        $(".caja-efectos").toggle("shake", 4000);
+        // $(".caja-efectos").toggle("scale");
+        // $(".caja-efectos").toggle("puff");
+        // $(".caja-efectos").toggle("fold");
+        // $(".caja-efectos").toggle("drop");
+        // $(".caja-efectos").toggle("slide");
+        // $(".caja-efectos").toggle("blind");
+        // $(".caja-efectos").toggle("explode");
+        // $(".caja-efectos").effect("explode");
+        // $(".caja-efectos").fadeToggle();
+    });
+
+});
