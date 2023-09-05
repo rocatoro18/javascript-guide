d2c0c2ece230c403d8a893a0663aad9a6c02728b
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-09-05 13:29:46 -0700

    const - let

---
+++ intro-javascript/src/basses/01_const-let.js
@@ -0,0 +1,19 @@
+
+// Variables y Constantes
+// YA NO SE USA VAR
+
+const nombre = 'Roberto';
+const apellido = 'Torres';
+
+let valorDado = 8;
+valorDado = 4;
+
+console.log(nombre,apellido,valorDado);
+
+if(true){
+    let valorDado = 6;
+    const nombre = 'Carlos';
+    console.log(valorDado,nombre);
+}
+
+console.log(valorDado);
